2d1697ec27f2d51dcd5740d8f0cde43fb6f50be6
aesthetiques <caleb.wells1@gmail.com>
2017-10-31 11:59:44 -0700

    Error fixed by VS restart? IsPalindrome complete.

---
+++ IsAPalindrome/IsAPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IsAPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsItAPalindrome(string word)
+            {
+                var isPal = true;
+                char[] wordArray = word.ToCharArray();
+                for(int i = 0; i < wordArray.Length / 2; i++)
+                {
+                    if(wordArray[i] != wordArray[wordArray.Length - i - 1])
+                    {
+                        isPal = false;
+                    }
+                }
+                return isPal;
+            }
+            Console.WriteLine(IsItAPalindrome("caleb"));
+        }
+    }
+}
