fef4e7e262e425a65f6253cf4f534a4f4991ab9f
caleb wells <caleb.wells1@gmail.com>
2017-10-31 11:00:27 -0700

    Did a thing

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FizzBuzz(int number)
+            {
+                return number % 15 == 0 ? "FizzBuzz" : number % 3 == 0 ? "Fizz" : number % 5 == 0 ? "Buzz" : $"Nope: {number}.";
+            }
+            Console.WriteLine(FizzBuzz(46));
+            Console.Read();
+        }
+    }
+}
